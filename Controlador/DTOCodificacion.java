583d19543c3699791e8848884b8097726f179b40
jose51197 <jose51197@hotmail.com>
2018-09-14 21:59:53 -0600

    Inicio de dto

--- Controlador/DTOCodificacion.java
+++ Controlador/DTOCodificacion.java
@@ -14,5 +14,20 @@ public class DTOCodificacion {
     private int idAlfabeto;
     private String entrada;
     private String salida;
-    
+
+    public String getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(String entrada) {
+        this.entrada = entrada;
+    }
+
+    public String getSalida() {
+        return salida;
+    }
+
+    public void setSalida(String salida) {
+        this.salida = salida;
+    }    
 }
