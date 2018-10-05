7a01273541bc8317350b9e4b1fb1aa948ed4dc31
jose51197 <jose51197@hotmail.com>
2018-10-05 00:57:52 -0600

    Genereador
    Generador*

--- Modelo/Generador.java
+++ Modelo/Generador.java
@@ -8,6 +8,7 @@ package Modelo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -16,27 +17,69 @@ import java.util.List;
 public class Generador {
     public List<Character> caracteres;
     public List<Integer> indices;
-    public short largo;
+    public int largo;
     
-    public String tiraUno(){
+    public String tiraUno(int pLargo){
+        String textoGenerado = "";
         List<Integer> indicesCopia = new ArrayList();
         Collections.copy(indicesCopia, this.indices);
+        Random rand = new Random();
+        int randIndex;
+        char last, current;
         
+        randIndex = rand.nextInt(indices.size() + 1);
+        last = this.caracteres.get( indices.get(randIndex) );
+            
+        textoGenerado+= last;
+            
+        indices.remove(randIndex);
+            
+        for (int agregados = 1; agregados < pLargo; agregados++){
+            randIndex = rand.nextInt(indices.size() + 1);
+            current = this.caracteres.get( indices.get(randIndex) );
+            
+            if (current == last + 1 ){
+                if (indices.size() == 1) 
+                    textoGenerado = current + textoGenerado;
+                else
+                    continue;
+            }
+            last = current;
+            textoGenerado+= this.caracteres.get( indices.get(randIndex) );
+            indices.remove(randIndex);
+        }
         
-       
-        return "";
+        return textoGenerado;
     }
     
-    public String tiraDos(){
+    public String tiraDos(int pLargo){
+        String textoGenerado = "";
         List<Integer> indicesCopia = new ArrayList();
         Collections.copy(indicesCopia, this.indices);
-       
-        return "";
+        Random rand = new Random();
+        int randIndex;
+        
+        for (int agregados = 0; agregados < pLargo; agregados++){
+            randIndex = rand.nextInt(indices.size() + 1);
+            textoGenerado+= this.caracteres.get( indices.get(randIndex) );
+            indices.remove(randIndex);
+        }
+        
+        return textoGenerado;
     }
     
-    public String tiraTres(){
+    public String tiraTres(int pLargo){
+        String textoGenerado = "";
         List<Integer> indicesCopia = new ArrayList();
         Collections.copy(indicesCopia, this.indices);
-        return "";
+        Random rand = new Random();
+        int randIndex;
+        
+        for (int agregados = 0; agregados < pLargo; agregados++){
+            randIndex = rand.nextInt(indices.size() + 1);
+            textoGenerado+= this.caracteres.get( indices.get(randIndex) );
+        }
+        
+        return textoGenerado;
     }
 }
--- Modelo/TipoCodificable.java
+++ Modelo/TipoCodificable.java
@@ -10,5 +10,5 @@ package Modelo;
  * @author sergiesalas
  */
 public enum TipoCodificable {
-    CodigoTelefonicos,Vigenere,Trasposicion
+    CodigoTelefonico,Vigenere,Trasposicion
 }
